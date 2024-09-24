ec5dbe425a27d62b58d1a0ae78e1ffd29f2c69ba
olamide <olamideashaolu318.gmail.com>
2024-09-24 03:28:57 +0100

    commit new codes

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,8 @@
  */
 import { sidebar } from "./sidebar.js";
 import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
-import {createMovieCard} from "./movie-card.js"
+import {createMovieCard} from "./movie-card.js";
+import { search } from "./search.js";
 
 const pageContent = document.querySelector('.page-content');//changed queryselector
 
@@ -210,3 +211,5 @@ const createMovieList = function({results: movieList}, title){
 
   pageContent.appendChild(movieListElem);
 }
+
+search();
--- assets/js/movie-list.js
+++ assets/js/movie-list.js
@@ -3,6 +3,7 @@
 import { api_key, fetchDataFromServer } from "./api.js";
 import { createMovieCard } from "./movie-card.js";
 import { sidebar } from "./sidebar.js";
+import { search } from "./search.js";
 
 //collect genreName and url params from local storage
 const genreName = window.localStorage.getItem('genreName');
@@ -73,3 +74,6 @@ fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie?${urlParam}&pag
 
 
 });
+
+
+search();
